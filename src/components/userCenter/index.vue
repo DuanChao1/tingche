d1ce4769bd8d9af4d4e220ccf57669cb8d4fcabd
duanchao5 <duanchao5@wanda.cn>
2017-07-11 16:23:38 +0800

    center

--- src/components/userCenter/index.vue
+++ src/components/userCenter/index.vue
@@ -1,8 +1,8 @@
 <template>
 	<header>
-		<span class="return"></span>
+		<span class="return" v-on:click="returnTo()"></span>
 		<p>个人中心</p>
-		<span class="refresh"></span>
+		<span class="refresh" v-on:click="refresh()"></span>
 	</header>
 	<section class="userInfo">
 		<div class="userMessage clx">
@@ -18,7 +18,29 @@
 			<p class="accountTitle">账户余额（小时）</p>
 			<p class="accountTime">20时50分</p>
 		</div>
-		<div class="rechargeBtn"></div>
+		<div class="rechargeBtn">
+			<span class="ico_money"></span>
+			去充值
+		</div>
+		<div class="bg"></div>
+	</section>
+	<section class="userMenu">
+		<div class="menuBox leftMenu">
+			<div class="park"><img src="../../../static/images/parkRecord.png" /></div>
+			<p>停车记录</p>
+		</div>
+		<div class="menuBox">
+			<div class="recharge"><img src="../../../static/images/rechargeRecord.png" /></div>
+			<p>充值记录</p>
+		</div>
+		<div class="menuBox leftMenu">
+			<div class="getcard"><img src="../../../static/images/card.png" /></div>
+			<p>领卡</p>
+		</div>
+		<div class="menuBox">
+			<div class="bindcard"><img src="../../../static/images/bindCard.png" /></div>
+			<p>绑卡</p>
+		</div>
 	</section>
 </template>
 <script>
@@ -31,13 +53,16 @@ export default{
   		document.setTitle('会员菜单')
   	},
    	methods: {
+   		returnTo () {
+   			window.history.back()
+   		},
+   		refresh () {
+   			window.location.reload()
+   		}
   	}
 }
 </script>
 <style scoped>
-body{
-	background: #eff3f8;
-}
 header{
 	position: relative;
 	height: .5rem;
@@ -117,7 +142,76 @@ header p{
     margin-left: -0.8rem;
 	width: 1.61rem;
 	height: .5rem;
+	line-height: .45rem;
+	text-align: center;
 	background: url('../../../static/images/recharge_bg.png');
 	background-size: 100% 100%;
+	z-index: 2;
+	font-size: .16rem;
+	color: #f16143;
+}
+.bg{
+	position: absolute;
+    bottom: -0.28rem;
+    left: 50%;
+    margin-left: -0.85rem;
+    width: 1.7rem;
+    height: .6rem;
+    background: #eff3f8;
+    border-radius: .26rem;
+}
+.ico_money{
+	display: inline-block;
+	width: .17rem;
+	height: .17rem;
+	background: url('../../../static/images/money.png');
+	background-size: 100% 100%;
+	vertical-align: -2px;
+}
+.userMenu{
+	margin-top: .3rem;
+	padding: 0 .15rem;
+}
+.userMenu .leftMenu{
+	margin-right: 2%;
+}
+.menuBox{
+	display: inline-block;
+	width: 48%;
+	height: 1.3rem;
+	background: #fff;
+	text-align: center;
+	border-radius: 0.06rem;
+	overflow: hidden;
+    margin-bottom: .05rem;
+}
+.menuBox p{
+	font-size: .15rem;
+	color: #666666;
+	margin-top: .12rem;
+}
+.park{
+	display: inline-block;
+	width: .64rem;
+	height: .69rem;
+	margin-top: .15rem;
+}
+.recharge{
+	display: inline-block;
+	width: .65rem;
+	height: .68rem;
+	margin-top: .15rem;
+}
+.getcard{
+	display: inline-block;
+	width: .77rem;
+	height: .59rem;
+	margin-top: .15rem;
+}
+.bindcard{
+	display: inline-block;
+	width: .68rem;
+	height: .61rem;
+	margin-top: .15rem;
 }
 </style>
