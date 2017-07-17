f40f5fbbe0a87fbc40962ec65264294f0755c6c5
duanchao5 <duanchao5@wanda.cn>
2017-07-17 17:39:54 +0800

    recharge

--- src/components/rechargeRecord/index.vue
+++ src/components/rechargeRecord/index.vue
@@ -7,7 +7,16 @@
 	<div class="record">
 		<div class="leftLine"></div>
 		<div class="record_list">
-			<p><span></span></p>
+			<div class="ico_recharge">
+				<div class="recharge"><img src="../../../static/images/ico_recharge.png" /></div>
+			</div>
+			<p><span>充值时间：</span>2017年05月03日15:36</p>
+			<div class="recordBox">
+				<div class="recordLeft">
+					<p class="price"><span>￥</span>350</p>
+					<p class="priceText">充值金额</p>
+				</div>
+			</div>
 		</div>
 	</div>
 </template>
@@ -47,5 +56,60 @@ export default{
 }
 .record_list{
 	margin-top: .28rem;
+}
+.recharge{
+	display: inline-block;
+	width: .19rem;
+	height: .15rem;
+}
+.ico_recharge{
+	display: inline-block;
+	width: .32rem;
+	height: .32rem;
+	text-align: center;
+	line-height: .28rem;
+    border: 1px solid #dbdbdb;
+    border-radius: .32rem;
+    margin-left: .16rem;
+    margin-right: .14rem;
+    background: #fff; 
+}
+.record_list>p{
+	display: inline-block;
+	font-size: .14rem;
+	color: #333;
+}
+.record_list>p>span{
+	color: #999;
+}
+.recordBox{
+	height: .9rem;
+	margin-left: .62rem;
+	margin-right: .15rem;
+	margin-top: .04rem;
+	background: url('../../../static/images/list_bg.png');
+	background-size: 100% 100%;
+}
+.recordBox .recordLeft{
+	float: left;
+    height: .9rem;
+    display: table;
+    width: 33%;
+    text-align: center;
+}
+.recordBox .recordLeft .price{
+	display: table-cell;
+    vertical-align: bottom;
+	color: #fa4a11;
+	font-size: .27rem;
+}
+.recordBox .recordLeft .price>span{
+	font-size: .15rem;
+}
+.recordBox .recordLeft .priceText{
+	display: table-row;
+    vertical-align: middle;
+    font-size: .1rem;
+    color: #999999;
 }
 </style>
